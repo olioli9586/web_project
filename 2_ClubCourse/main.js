73232f52c12d52239861533bacc6cbda6e02bf83
olioli9586 <76279803+olioli9586@users.noreply.github.com>
2023-04-15 23:50:31 +0800

    update club course

--- 2_ClubCourse/main.js
+++ 2_ClubCourse/main.js
@@ -1,26 +1,70 @@
+var topic = [
+    "School has not yet started",
+    "National Holiday",
+    "Prepare environment",
+    "Randomness",
+    "Repeatability"
+];
 
-$(function(){
+
+$("#startDate").on("change", function() {
+// Get the selected date from the input field
+var startDate = new Date($(this).val());
+
+var millisecsPerDay = 24 * 60 * 60 * 1000;
+
+// Clear the table
+$("#courseTable").empty();
+
+// Add table header
+$("#courseTable").append(
+    "<tr><th>Sessions</th><th>Time</th><th>Theme</th></tr>"
+);
+
+// Generate course schedule
+var topicCount = topic.length;
+for (var x = 0; x < topicCount; x++) {
+    var rowClass = x % 2 === 0 ? "even-row" : "odd-row";
+    
+    if (topic[x] === "National Holiday") {
+        rowClass = "grey-row"; // Set row class to "grey-row"
+    }
+
     $("#courseTable").append(
-    "<tr> <th>場次</th> <th>時間</th> <th>主題</th> </tr>");
-
-    //一秒鐘有1000毫秒
-    //每分鐘60秒、每小時60分鐘、每天24小時
-    let millisecsPerDay = 24 * 60 * 60 * 1000;
-
-    var topicCount= topic.length;
-    for(var x = 0; x < topicCount; x++){
-        $("#courseTable").append(
-            "<tr>"+
-            `<td>${x+1}</td>`+
-            `<td>${(new Date(startDate.getTime() + 7 * x * millisecsPerDay)).toLocaleDateString()}</td>`+
-            `<td>${topic[x]}</td>`+
-            "</tr>");
+        `<tr class="${rowClass}">` +
+        "<td>" + (x + 1) + "</td>" +
+        "<td>" +
+        new Date(
+            startDate.getTime() + 7 * x * millisecsPerDay
+        ).toLocaleDateString(undefined, {
+            month: "numeric",
+            day: "numeric"
+        }) +
+        "</td>" +
+        "<td>" + topic[x] + "</td>" +
+        "</tr>"
+        );
     }
 });
 
-//extra pratice
+// Event listener for add button
+$("#addBtn").on("click", function() {
+    let newTopic = $("#newTopicInput").val(); // Get value from input field
+    if (newTopic !== "") { // Check if input field is not empty
+        topic.push(newTopic); // Add new topic to topic array
+        updateTable(); // Update table with new data
+        $("#newTopicInput").val(""); // Clear input field
+    }
 
-//get rid of year, only leave month and date
-//get user to input start date
-//if is dayoff, set that row to gray
-//odd row and even row set to different color
+});
+
+$(document).ready(function() {
+    // Show the modal window on page load
+    $("#myModal").css("display", "block");
+  
+    // Add click event listener for the close button
+    $(".close").click(function() {
+        // Hide the modal window when close button is clicked
+        $("#myModal").css("display", "none");
+    });
+});
